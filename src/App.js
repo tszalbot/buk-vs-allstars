6271d02e77d6745f69afa57ed76ed752b8fa6fdd
Tobiasz Szalbot <tobiasz.szalbot@rocksoft.pl>
2020-01-10 08:03:58 +0100

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,71 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 
+class Body extends Component {
+    constructor() {
+        super();
+        this.state = {
+            buk: 0,
+            as: 0
+        }
+    }
+
+    componentDidMount() {
+        fetch('https://script.google.com/macros/s/AKfycbyGZjGb1qaHJ6W4cfZE3lUJlMfwJC1fgapQqc56Bs_3NhOVohU/exec')
+        .then(results => {
+            return results.json();
+        })
+        .then(data => {
+            this.setState({
+                buk: data['BUK'].toFixed(2) + '%',
+                as: data['AllStars'].toFixed(2) + '%',
+            })
+        })
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <div className="main-div buk-div">
+                    <div className="image">
+                        <div className="img-wrapper">
+                            <img className="buk-img" src="buk.svg" alt=""/>
+                        </div>
+                    </div>
+
+                    <ProgressBar width={this.state.buk} color="white"/>
+                </div>
+
+                <div className="main-div">
+                    <div className="image">
+                        <div className="img-wrapper">
+                            <img src="allstars.svg" alt=""/>
+                        </div>
+                    </div>
+
+                    <ProgressBar width={this.state.as} color="black"/>
+                </div>
+            </div>
+        )
+    }
+}
+
+function ProgressBar(props) {
+    return (
+        <div className="ps-div">
+            <div className="progress-bar-wrapper">
+                <div className="progress-bar" style={{width: props.width}}></div>
+            </div>
+
+            <div className={'ps-text ' + 'ps-text-' + props.color}>
+                {props.width}
+            </div>
+        </div>
+    )
+}
+
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return <Body/>
 }
 
 export default App;
